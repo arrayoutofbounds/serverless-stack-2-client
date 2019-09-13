39e4976a0977830f9faf92cecad2eec9d44484e2
Anmol Desai <anmoldesai4@gmail.com>
2019-09-13 16:12:24 +1000

    add dev and prod env

--- src/config.js
+++ src/config.js
@@ -1,18 +1,44 @@
+const dev = {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-17y3pwqq2bjf"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://c25g95yuzj.execute-api.ap-southeast-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_69IPeidpz",
+    APP_CLIENT_ID: "bpakhoibf1qodl530lafvdi2j",
+    IDENTITY_POOL_ID: "ap-southeast-2:0d070093-680b-4db9-ac6d-ee6bfbe2ee27"
+  }
+};
+
+const prod = {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-jml4kw5e0itt"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://2udrwnawji.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_oaxep6LyF",
+    APP_CLIENT_ID: "77ntskc6c2cjkgenn3djjgjvot",
+    IDENTITY_POOL_ID: "ap-southeast-2:ad413222-9c05-4f3b-8168-8b920008373a"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
 export default {
-    s3: {
-      REGION: "ap-southeast-2",
-      BUCKET: "notes-app-uploads-anmoldesai"
-    },
-    apiGateway: {
-      REGION: "ap-southeast-2",
-      URL: "https://jvj44ezzt9.execute-api.ap-southeast-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "ap-southeast-2",
-      USER_POOL_ID: "ap-southeast-2_zIbIxMOFX",
-      APP_CLIENT_ID: "165vl9vm0hlo8co7uvau8j9k99",
-      IDENTITY_POOL_ID: "ap-southeast-2:133f25b6-60d1-475c-b614-5f12aff67e7c"
-    },
-    MAX_ATTACHMENT_SIZE: 5000000,
-  };
-  
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
